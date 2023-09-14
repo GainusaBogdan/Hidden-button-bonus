119dbda9a592f8d6aca80e2b52454968b0f17bb2
GainusaBogdan <130216860+GainusaBogdan@users.noreply.github.com>
2023-09-14 18:13:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-(function(){
-  GenerateButtons();
-})();
-//Generate buttons with prize!
-function GenerateButtons() {
+generateButtons();
+
+//Generate buttons!
+function generateButtons() {
   const numberofbuttons = document.getElementById("imputvalue").value;
   const myprize= new Array("Winner !");
-  for(i = 0; i < numberofbuttons - 1; ++i) {
+  for(i = 0; i < numberofbuttons; ++i) {
     const button = document.createElement('button');
-      myprize.push("Loser !");
-      const message = myprize.splice(Math.floor(Math.random() * myprize.length),1);
-      button.innerHTML = message;
+    myprize.push("Loser !");
       button.className = "btn btn-warning btn-lg btn-block border-light text-dark";
       document.body.appendChild(button);
       button.addEventListener('click', () => {
-        alert(message);
-      });
+        prize(myprize);
+      }, {once : true});
   }
-  if (numberofbuttons > 1) {
-    const button = document.createElement('button');
-      const message = myprize.splice(Math.floor(Math.random() * myprize.length),1);
-      button.innerHTML = message;
-      button.className = "btn btn-warning btn-lg btn-block border-light text-dark";
-      document.body.appendChild(button);
-      button.addEventListener('click', () => {
-        alert(message);
-      });
-    }
+  myprize.pop();
+}
+
+//Find out the prize!
+function prize(myprize) {
+  this.myprize = myprize;
+  const mesage = myprize.splice(Math.floor(Math.random() * myprize.length),1);
+  alert(mesage);
 }
